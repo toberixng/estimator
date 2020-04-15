5cadce1f4d6c99da9d5eb3c4ed6043ce51e50ab4
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-04-15 13:22:24 +0100

    public folder enabled

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require ('express');
+const path = require('path');
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
-});
+// static filder
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello World</h1>')
+// });
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000;
 
