5679f16ddb96ef950475f804346230c100c8dd36
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-04-20 15:22:07 +0100

    meta description

--- src/estimator.js
+++ src/estimator.js
@@ -73,3 +73,64 @@ const covid19ImpactEstimator = (data) => {
 };
 
 export default covid19ImpactEstimator;
+
+const form = document.querySelector('#form');
+const population = document.querySelector('#population');
+const timeToElapse = document.querySelector('#timeToElapse');
+const reportedCases = document.querySelector('#reportedCases');
+const totalHospitalBeds = document.querySelector('#totalHospitalBeds');
+const periodType = document.querySelector('#periodType');
+
+
+// Show input Error Message
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+const showSuccess = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success'; 
+}
+
+// check each of the field for success or error input
+const checkRequired = (inputArr) => {
+  inputArr.forEach((input) => {
+    return (input.value.trim() === '') ?  
+    showError(input, 'This Field is required') :
+    showSuccess(input);
+  })
+};
+
+const addValue = (e) => {
+  e.preventDefault()
+  const data = {
+    region: {
+      name: "Africa",
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+      },
+    reportedCases: +reportedCases.value,
+    timeToElapse: +timeToElapse.value,
+    periodType: periodType.options[periodType.selectedIndex].value,
+    totalHospitalBeds: +totalHospitalBeds.value,
+    population: +population.value, 
+  }
+
+  
+  checkRequired([population, timeToElapse, reportedCases, totalHospitalBeds, periodType]);
+
+  reportedCases.value = ''
+  timeToElapse.value = ''
+  totalHospitalBeds.value = ''
+  population.value = ''
+
+  console.log(covid19ImpactEstimator(data))
+}
+
+form.addEventListener('submit', addValue)
+
